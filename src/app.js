fb0a379c0d92994e71342dcb8dd2d91ecf96c4ec
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-23 10:59:51 +0700

    success to connect to google 3rd party

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 import router from './routes.js';
 
@@ -15,6 +16,25 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // body parser
 app.use(express.json());
+
+// nademailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_PW_APP,
+  },
+});
+
+// testing transport nodemailer
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready for messages');
+    console.log(success);
+  }
+})
 
 // routes
 app.use(router);
